f755cb3674c0add620701825968bb5c4e7add681
Laura Lund <lundl@flip1.engr.oregonstate.edu>
2019-02-05 22:31:02 -0800

    cardtest1 almost done

--- projects/lundl/dominion/cardtest1.c
+++ projects/lundl/dominion/cardtest1.c
@@ -25,11 +25,11 @@ void printResults(char* testCase, int assertResult)
 {
     if(assertResult == 0)
     {
-	printf("shuffle(): FAIL %s\n", testCase);
+	printf("smithy: FAIL %s\n", testCase);
     }
     else
     {
-	printf("shuffle(): PASS %s\n", testCase);
+	printf("smithy: PASS %s\n", testCase);
     }
 }
 
@@ -68,242 +68,111 @@ void testSmithy()
     // Initialize a gameState struct with values we need for this test
     struct gameState* testGame = newGame();
     testGame->numPlayers = 2;
+
+    // set player 0 in whoseTurn
+    testGame->whoseTurn = 0;
+
+    // initialize deck and hand counts
     testGame->deckCount[0] = 0;
+    testGame->handCount[0] = 0;
 
     // This gameStruct pointer is used for copies
-    struct gameState* testCopy = newGame();
+   // struct gameState* testCopy = newGame();
  
     // Set up deck for player 0
+
+    // Deck holds all different values except smithy
+    int i;
+    for(i=curse; i <= treasure_map ; i++)
+    {
+	if (i != smithy)
+	{
+	    testGame->deck[0][i] = i;
+	    testGame->deckCount[0] += 1;
+	}
+    }
+
+
+    /* Test Case 1: smithy in position 4 */
     // Set up hand for player 0
+    printf("smithy Test Case 1\n"); 
+    for(i = 0; i < 4; i++)
+    {
+	// draw card from deck
+	drawCard(testGame->whoseTurn, testGame);	
+    }
+
+    // put smithy in position 4
+    testGame->hand[0][4] = smithy;
+    testGame->handCount[0]++;
+    int handPos = 4;
+
     // Set up deck for player 1
     // Set up hand for player 1
-    // set this player in whoseTurn
     // set card = smithy
-    // set hand position (what is this?)
+    int card = smithy;
 
- 
-    //set up random number generator
-    int randomSeed = time(NULL) % 86400;
-    SelectStream(1);
-    PutSeed((long)randomSeed);
-  
-
-    /* Test Case 1: 3 estate, 7 copper 
-     * The original order is 3 estate, 7 copper. We expect shuffle to change
-     * this to some other order. */
-
-    printf("\nshuffle() Test Case 1\n");
-
-    // Set up deck for player 0 
-    int i;
-    for(i = 0; i < 3; i++)
+  /*  printf("current hand:\n");
+    for(i=0; i < testGame->handCount[0]; i++)
     {
-	testGame->deck[0][i] = estate;
-        testGame->deckCount[0]++;
+	printf("Hand at position %i is %i\n", i, testGame->hand[0][i]);
     }
-    for(i = 3; i < 10; i++)
-    {
-        testGame->deck[0][i] = copper;
-	testGame->deckCount[0]++;
-    }
+   */
+    // Save current hand count
+    int handCountBefore = testGame->handCount[0];
+    //printf("handCount: %i\n", handCount);
 
-    // Save a copy of this test struct
-    memcpy(testCopy->deck[0], testGame->deck[0], sizeof(testGame->deck[0]));
-    testCopy->deckCount[0] = testGame->deckCount[0];
-
-    // Pass this gameState struct along with a valid player num to shuffle()
-    int returnVal = shuffle(0, testGame);
-
+    // call cardEffect
+    int returnVal = cardEffect(card, 0, 0, 0, testGame, handPos, 0);
+    
+    // Assert that handCount is 3 - 1 more than current handCount and that resultVal is 0
     // Check the restults of this test case
-    char* testCase1 = "shuffling deck of 3 estate and 7 copper";
+    char* testCase1 = "smithy effect with hand count of 5 and smithy position of 4";
 
-    // Assert that the two decks differ and that shuffle returnval is 0
-    if(assertTrue(checkDeckDifferences(testGame, testCopy), 1, returnVal, 0))
+    // Assert that the handcount is 3 - 1 more than current hand count and that cardEffect returnval is 0
+    if(assertTrue(testGame->handCount[0], handCountBefore + 3 - 1, returnVal, 0))
     {
 	printResults(testCase1, 1);
     }
     else
     {
+	printf("actual handcount: %i, expected handcount: %i\n", testGame->handCount[0], handCountBefore + 3 - 1);
 	printResults(testCase1, 0);
     }
 
-    /* Test Case 2: an empty deck, deckCount 0 
-     * We expect that shuffle() will return -1. */
 
-    printf("\nshuffle() Test Case 2\n");
+    // set hand position for smithy so that it can be discarded successfully
+    // have other cards in hand along with smithy
+    // test smithy in different hand positions
+    // check number of cards before and after playing smithy
+    // test playing one smithy while also having other smithy cards in the hand
 
-    // Edge Case: deck is empty, pass if returns -1; fail if returns 0
  
-    testGame->deckCount[0] = 0;
-
-    returnVal = shuffle(0, testGame);
-
-    // Check the restults of this test case
-    char* testCase2 = "shuffling empty deck";
-
-    // Assert that the returnval is -1
-    if(assertTrue(0, 0, returnVal, -1))
-    {
-	printResults(testCase2, 1);
-    }
-    else
-    {
-	printResults(testCase2, 0);
-    }
-
-    /* Test Case 3: 20 of the same card
-     * We expect the shuffled order and the original order to be the same. */
-
-    printf("\nshuffle() Test Case 3\n");
-    char* testCase3 = "shuffling deck of 20 gardens";
-
-    // Deck holds all same values
-    for(i=0; i < 20; i++)
-    {
-	testGame->deck[0][i] = gardens;
-    }
-    testGame->deckCount[0] = 20;
-
-    // Save a copy of this test struct
-    memcpy(testCopy->deck[0], testGame->deck[0], sizeof(testGame->deck[0]));
-    testCopy->deckCount[0] = testGame->deckCount[0];
-
-
-    // Pass this gameState struct along with a valid player num to shuffle()
-    returnVal = shuffle(0, testGame);
-
-    // Check the results of this test case
-
-    // Assert that the two decks are the same and that shuffle returnval is 0
-    if(assertTrue(checkDeckDifferences(testGame, testCopy), 0, returnVal, 0))
-    {
-	printResults(testCase3, 1);
-    }
-    else
-    {
-	printResults(testCase3, 0);
-    }
-
-   /* Test Case 4: a deck comprised of one of each card
-    * The original order is the same as the enum list (curse, estate, etc.)
-    * We expect the shuffled order to be in any other order. */
-
-    printf("\nshuffle() Test Case 4\n");
-    char* testCase4 = "shuffling deck of one of each card";
-
-    // reset deck count
-    testGame->deckCount[0]=0;
-
-    // Deck holds all different values
-    for(i=curse; i <= treasure_map ; i++)
-    {
-	testGame->deck[0][i] = i;
-	testGame->deckCount[0] += 1;
-    }
+    //set up random number generator
+ //   int randomSeed = time(NULL) % 86400;
+ //   SelectStream(1);
+ //   PutSeed((long)randomSeed);
   
 
-    // Save a copy of this test struct
-    memcpy(testCopy->deck[0], testGame->deck[0], sizeof(testGame->deck[0]));
-    testCopy->deckCount[0] = testGame->deckCount[0];
- 
+      // Save a copy of this test struct
+  //  memcpy(testCopy->deck[0], testGame->deck[0], sizeof(testGame->deck[0]));
+ //   testCopy->deckCount[0] = testGame->deckCount[0];
 
     // Pass this gameState struct along with a valid player num to shuffle()
-    returnVal = shuffle(0, testGame);
-
-    // Check the results of this test case
-
-    // Assert that the two decks are different and that shuffle returnval is 0
-    if(assertTrue(checkDeckDifferences(testGame, testCopy), 1, returnVal, 0))
-    {
-	printResults(testCase4, 1);
-    }
-    else
-    {
-	printResults(testCase4, 0);
-    }
-
-
-   /* Test Case 5: a max size deck holding at least one of each card 
-    * The original order is the repeated pattern of curse ... treasure_map
-    * until 500 cards are stored in the deck. We expect the shuffled order
-    * to be any other order.*/
-  
-    printf("\nshuffle() Test Case 5\n");
-    char* testCase5 = "shuffling max size deck of assorted cards";
-
-    // reset deck count
-    testGame->deckCount[0]=0;
-
-    // Deck holds 500 cards of assorted values
-    i = 0;
-    while(i < MAX_DECK)
-    {
-	int j;
-        for(j = curse; j <= treasure_map; j++)
-	{
-	    if(i < MAX_DECK)
-	    {
-		testGame->deck[0][i] = j;
-		testGame->deckCount[0] += 1;
-	        i++;
-            }
-            else
-	    {
-		break;
-	    }
-	}
-    }
-  
-
-    // Save a copy of this test struct
-    memcpy(testCopy->deck[0], testGame->deck[0], sizeof(testGame->deck[0]));
-    testCopy->deckCount[0] = testGame->deckCount[0];
- 
-
-    // Pass this gameState struct along with a valid player num to shuffle()
-    returnVal = shuffle(0, testGame);
+ //   int returnVal = shuffle(0, testGame);
 
     // Check the restults of this test case
+//    char* testCase1 = "shuffling deck of 3 estate and 7 copper";
 
-    // Assert that the two decks are different and that shuffle returnval is 0
-    if(assertTrue(checkDeckDifferences(testGame, testCopy), 1, returnVal, 0))
-    {
-	printResults(testCase5, 1);
-    }
-    else
-    {
-	printResults(testCase5, 0);
-    }
-    
-    /* Test Case 6: a deck of one card
-     * The original order is baron. We expect the shuffled order to also be
-     * a single baron. */
-    printf("\nshuffle() Test Case 6\n");
-
-    // Edge Case: deck holds 1 card, pass if returns 0; fail if returns -1
-    testGame->deck[0][0] = baron; 
-    testGame->deckCount[0] = 1;
-
-    // Save a copy of this test struct
-    memcpy(testCopy->deck[0], testGame->deck[0], sizeof(testGame->deck[0]));
-    testCopy->deckCount[0] = testGame->deckCount[0];
- 
-    returnVal = shuffle(0, testGame);
-
-    // Check the restults of this test case
-    char* testCase6 = "shuffling deck of 1 card";
-
-    // Assert that the decks match and returnval is 0
-    if(assertTrue(checkDeckDifferences(testGame, testCopy), 0, returnVal, 0))
-    {
-	printResults(testCase6, 1);
-    }
-    else
-    {
-	printResults(testCase6, 0);
-    }
-
-
+    // Assert that the two decks differ and that shuffle returnval is 0
+//    if(assertTrue(checkDeckDifferences(testGame, testCopy), 1, returnVal, 0))
+//    {
+//	printResults(testCase1, 1);
+//    }
+//    else
+//    {
+//	printResults(testCase1, 0);
+//    }
 
 }
 
@@ -313,6 +182,6 @@ void testSmithy()
 
 int main(int argc, char *argv[])
 {
-    testShuffle();
+    testSmithy();
     return 0;
 }
